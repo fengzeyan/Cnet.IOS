a2d365b59d2115d5b0e04071df4af0ef58e0eea1
Scott Babcock <scott.babcock@onsharp.com>
2014-04-04 11:59:41 -0500

    Created Authentication Helper
    Created an authentication helper class that stores username in user
    defaults and password in a keychain.
    Updated constants to point to preview.

---
+++ src/Cnet.iOS/AuthenticationHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MonoTouch.Foundation;
+using MonoTouch.Security;
+using MonoTouch.UIKit;
+using Cnt.API;
+
+namespace Cnet.iOS
+{
+	public static class AuthenticationHelper
+	{
+		public static Client GetClient()
+		{
+			string username = NSUserDefaults.StandardUserDefaults.StringForKey("username");
+			string password = KeychainHelpers.GetPasswordForUsername (username, Constants.NTMOBILE_APPLICATION_ID, true);
+			return new Client (username, password);
+		}
+
+		public static void Authenticate(string username, string password)
+		{
+			NSUserDefaults.StandardUserDefaults.SetString(username, "username");
+			KeychainHelpers.SetPasswordForUsername(username, password, Constants.NTMOBILE_APPLICATION_ID, SecAccessible.Always, true);
+
+			string deviceId = UIDevice.CurrentDevice.IdentifierForVendor.AsString();
+			Client client = new Client(username, password);
+			client.AuthenticateService.Authenticate(deviceId);
+		}
+	}
+}
+
